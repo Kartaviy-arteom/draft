2980a413a4daaa10607e1d28f4793241e981775f
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2019-12-23 16:39:30 +0300

    Create sliders.js

---
+++ source/js/modules/sliders.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function () {
+
+var headerLineSlider = new Swiper('.page-header__line.swiper-container', {
+    updateOnWindowResize: true,
+    speed: 400,
+    slidesPerView: 8,
+    slidesPerGroup: 1,
+    loop: true,
+  });
+})();
