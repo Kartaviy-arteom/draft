b8538e1d3c73a7f8e1d570ab952b58b262e11c10
Kartaviy-arteom <kartaviy_arteom@mail.ru>
2019-12-24 12:48:13 +0300

    промо слайдер

--- build/js/modules/sliders.js
+++ build/js/modules/sliders.js
@@ -2,11 +2,24 @@
 
 (function () {
 
-var headerLineSlider = new Swiper('.page-header__line.swiper-container', {
+  var headerLineSlider = new Swiper('.page-header__line.swiper-container', {
     updateOnWindowResize: true,
     speed: 400,
     slidesPerView: 8,
     slidesPerGroup: 1,
     loop: true,
+  });
+
+  var promoSlider = new Swiper('.promo__slider.swiper-container', {
+    speed: 400,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".promo__slider-btn.swiper-button-next",
+      prevEl: ".promo__slider-btn .swiper-button-prev"
+    },
+    pagination: {
+      el: '.promo__slider-pagination.swiper-pagination',
+      clickable: true,
+    },
   });
 })();
